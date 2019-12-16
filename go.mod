da7f539c40daa1753ca23006d159002f2e52d213
leeqiang250 <leeqiang250@163.com>
2019-12-16 17:24:08 +0800

    BUG修复。

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-client
 
 go 1.13
+
+require github.com/leeqiang250/go-common v0.0.0-20191216092140-4ee5a330de5c
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/leeqiang250/go-common/common"
+)
+
+func main() {
+	common.Test0()
+}
